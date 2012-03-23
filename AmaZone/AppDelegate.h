aae309995cd184fb9751056d635de4e831fb056d
Jonathan Taylor <jonathantaylor@Jonathan-Taylors-iMac.local>
2012-03-23 18:41:23 +0000

    Initial Commit

---
+++ AmaZone/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AmaZone
+//
+//  Created by Jonathan Taylor on 23/03/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
